c212dca3e08d1aaaf3f9fa7d6930c913452cd97e
flamrdevs <flamrdevs@gmail.com>
2024-05-17 02:35:47 +0700

    initial app

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <main
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100dvw",
+          height: "100dvh",
+        }}
+      >
+        <Typography variant="h2">demo-jsonplaceholder-react</Typography>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import App from "~/App.tsx";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
